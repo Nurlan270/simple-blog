7b4ac04446ff9558b759b24e3bf18b23a40d628c
Nurlan270 <nurlanismzade@gmail.com>
2024-07-12 14:24:59 +0400

    added more statistics in admin panel

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -38,7 +38,7 @@
         @yield('page.title') | {{ config('app.name') }}
     </title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
